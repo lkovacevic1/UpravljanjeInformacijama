00cb47dddcb2999d519616206fe3657abcd59e57
Hp <Hp@DESKTOP-K4F0ROU>
2020-10-23 14:14:23 +0200

    Dodavanje entiteta Nastavnik.

---
+++ studsluzba/src/main/java/studsluzba/model/Nastavnik.java
@@ -0,0 +1,115 @@
+package studsluzba.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+/*
+@Entity
+@NamedQuery(name="Nastavnik.findAll", query="SELECT n FROM Nastavnik n")
+public class Nastavnik implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idNastavnik;
+	
+	private String ime;
+	private String prezime;
+	private String srednjeIme;
+	private String email;
+	private String zavrsenaVisokoskolskaUstanova;
+	
+	@ManyToOne
+	@JoinColumn(name = "idZvanja")
+	private Zvanje zvanje;
+
+	public Nastavnik() {
+		
+	}
+	
+	public Nastavnik(int idNastavnik, String ime, String prezime, String srednjeIme, String email,
+			String zavrsenaVisokoskolskaUstanova, Zvanje zvanje) {
+		super();
+		this.idNastavnik = idNastavnik;
+		this.ime = ime;
+		this.prezime = prezime;
+		this.srednjeIme = srednjeIme;
+		this.email = email;
+		this.zavrsenaVisokoskolskaUstanova = zavrsenaVisokoskolskaUstanova;
+		this.zvanje = zvanje;
+	}
+
+	public int getIdNastavnik() {
+		return idNastavnik;
+	}
+
+	public void setIdNastavnik(int idNastavnik) {
+		this.idNastavnik = idNastavnik;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getSrednjeIme() {
+		return srednjeIme;
+	}
+
+	public void setSrednjeIme(String srednjeIme) {
+		this.srednjeIme = srednjeIme;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getZavrsenaVisokoskolskaUstanova() {
+		return zavrsenaVisokoskolskaUstanova;
+	}
+
+	public void setZavrsenaVisokoskolskaUstanova(String zavrsenaVisokoskolskaUstanova) {
+		this.zavrsenaVisokoskolskaUstanova = zavrsenaVisokoskolskaUstanova;
+	}
+
+	public Zvanje getZvanje() {
+		return zvanje;
+	}
+
+	public void setZvanje(Zvanje zvanje) {
+		this.zvanje = zvanje;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Nastavnik [idNastavnik=" + idNastavnik + ", ime=" + ime + ", prezime=" + prezime + ", srednjeIme="
+				+ srednjeIme + ", email=" + email + ", zavrsenaVisokoskolskaUstanova=" + zavrsenaVisokoskolskaUstanova
+				+ "]";
+	}
+
+}*/
