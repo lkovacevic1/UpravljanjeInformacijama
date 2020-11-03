bf3304f27d07f615444ddaad3450b88c26a385bf
Name <Name@DESKTOP-J2IBVKN>
2020-11-03 21:41:23 +0100

    Obrisani podaci

--- studsluzba/src/test/java/studsluzba/repositorytest/StudentRepositoryTest.java
+++ studsluzba/src/test/java/studsluzba/repositorytest/StudentRepositoryTest.java
@@ -1,6 +1,7 @@
 package studsluzba.repositorytest;
 
 import java.text.ParseException;
+import java.time.LocalDate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,6 +14,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import studsluzba.model.Indeks;
+import studsluzba.model.SrednjaSkola;
+import studsluzba.model.Student;
+import studsluzba.model.VisokoskolskaUstanova;
 import studsluzba.repositories.IndeksRepository;
 import studsluzba.repositories.SrednjaSkolaRepository;
 import studsluzba.repositories.StudentRepository;
@@ -37,25 +41,5 @@ public class StudentRepositoryTest {
 	@Test
 	public void saveStudentTest() throws ParseException {
 		
-		/*Indeks i = new Indeks();
-		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("upinf");
-		EntityManager em = emf.createEntityManager();
-		
-		try {
-			em.getTransaction().begin();
-			
-			i = em.find(Indeks.class, 1);
-			System.out.println(i.getBrojIndexa());
-			i.setAktivan(false);
-			
-			em.persist(i);
-			
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			em.close();
-		}*/
-	}
+    }
 }
