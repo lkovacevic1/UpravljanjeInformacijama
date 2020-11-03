b38bc3aca225ca96faf154e14e1d03171644441c
Name <Name@DESKTOP-J2IBVKN>
2020-11-03 21:32:06 +0100

    kometari

--- studsluzba/src/test/java/studsluzba/repositorytest/StudentRepositoryTest.java
+++ studsluzba/src/test/java/studsluzba/repositorytest/StudentRepositoryTest.java
@@ -1,11 +1,18 @@
 package studsluzba.repositorytest;
 
 import java.text.ParseException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import studsluzba.model.Indeks;
 import studsluzba.repositories.IndeksRepository;
 import studsluzba.repositories.SrednjaSkolaRepository;
 import studsluzba.repositories.StudentRepository;
@@ -30,7 +37,7 @@ public class StudentRepositoryTest {
 	@Test
 	public void saveStudentTest() throws ParseException {
 		
-		Indeks i = new Indeks();
+		/*Indeks i = new Indeks();
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("upinf");
 		EntityManager em = emf.createEntityManager();
@@ -49,6 +56,6 @@ public class StudentRepositoryTest {
 			e.printStackTrace();
 		}finally {
 			em.close();
-		}
+		}*/
 	}
 }
