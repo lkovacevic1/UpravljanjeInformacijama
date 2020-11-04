97f262ece5ea4167e2bc05fcb9bc42bb62b300e0
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2020-11-04 21:15:59 +0100

    1243415

--- studsluzba/src/main/java/studsluzba/repositories/StudentRepository.java
+++ studsluzba/src/main/java/studsluzba/repositories/StudentRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import studsluzba.model.ObnovaGodine;
+import studsluzba.model.PolozenPredmet;
 import studsluzba.model.Student;
+import studsluzba.model.UpisGodine;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
+
 	
-	//selekcija studenta preko id
-	@Query("select s form Student s join fetch s.indeks i where i.idIndeks like :id")
-	Student findStudentByID(int id);
-	
+
 }
