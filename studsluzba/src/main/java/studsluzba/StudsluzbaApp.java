04c7976a6e172b4e484ae9cff2e93c9454d2f60a
Anastasija Radonjic <anastasijaradonjic99@gmail.com>
2020-11-27 23:11:07 +0100

    Napravljen model korisnickog interfejsa.

--- studsluzba/src/main/java/studsluzba/StudsluzbaApp.java
+++ studsluzba/src/main/java/studsluzba/StudsluzbaApp.java
@@ -2,14 +2,43 @@ package studsluzba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import studsluzba.client.MainViewManager;
+
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class StudsluzbaApp {
+public class StudsluzbaApp extends Application{
+	
+	protected ConfigurableApplicationContext springContext;
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudsluzbaApp.class, args);
+		launch(StudsluzbaApp.class);
 	}
 
+	@Override
+	public void init() {
+		springContext = SpringApplication.run(StudsluzbaApp.class);
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("RAF studentska sluzba");
+		MainViewManager mainView = springContext.getBean(MainViewManager.class);
+		primaryStage.setScene(mainView.createScene());
+		primaryStage.show();
+	}
+
+	@Override
+	public void stop() {
+		springContext.close();
+		Platform.exit();
+	}
+	
 }
