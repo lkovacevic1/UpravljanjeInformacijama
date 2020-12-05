12a85f45e1a1703fba9a33114d0106d863a36b69
lkovacevic1 <lukablok4598@gmail.com>
2020-12-05 20:10:39 +0100

    Dodata forma

--- studsluzba/src/main/java/studsluzba/client/fxmlcontrollers/SortStudentaByIndeksController.java
+++ studsluzba/src/main/java/studsluzba/client/fxmlcontrollers/SortStudentaByIndeksController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import studsluzba.client.MainViewManager;
 import studsluzba.model.Student;
 import studsluzba.repositories.StudentRepository;
 import studsluzba.services.StudentService;
@@ -25,7 +26,11 @@ public class SortStudentaByIndeksController {
 	 
 	 @Autowired
 	 StudentRepository studentRepo;
+	 
+	 @Autowired  
+	 MainViewManager mainViewManager;
 		
+	 Student selektovanStudent;
 		
 	 @FXML private TextField indeks;
 	 
@@ -36,6 +41,24 @@ public class SortStudentaByIndeksController {
 	 @FXML
 	 protected void initialize() {
 		 sviStidenti = FXCollections.observableList(studentService.loadAll());
+		 
+		 studentiTable.setRowFactory( tv -> {
+			    TableRow<Student> row = new TableRow<>();
+			    row.setOnMouseClicked(event -> {
+			        if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+			            Student rowData = row.getItem();
+			            /*System.out.println(rowData);
+			            
+			            Student st = row.getItem();
+			            System.out.println(st.getIdstudent());*/
+			            selektovanStudent = rowData;
+			            
+			            mainViewManager.openModal("TabAktivnostIndeksStudenta", 750, 500);
+			        }
+			    });
+			    return row ;
+			});
+		 
 		 studentiTable.setItems(sviStidenti);
 	 }
 		
