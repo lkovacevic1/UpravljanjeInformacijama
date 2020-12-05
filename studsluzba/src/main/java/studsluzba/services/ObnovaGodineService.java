3ec26d128a89e7f295209e79b69a1c89c7100546
lkovacevic1 <lukablok4598@gmail.com>
2020-12-05 17:33:16 +0100

    Popravljena obnova godine. Dodato sacuvavanje indeksa

--- studsluzba/src/main/java/studsluzba/services/ObnovaGodineService.java
+++ studsluzba/src/main/java/studsluzba/services/ObnovaGodineService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import studsluzba.model.Indeks;
 import studsluzba.model.ObnovaGodine;
 import studsluzba.model.Predmet;
+import studsluzba.repositories.IndeksRepository;
 import studsluzba.repositories.ObnovaGodineRepository;
 
 
@@ -17,6 +18,9 @@ public class ObnovaGodineService {
 	@Autowired 
 	ObnovaGodineRepository obnovaGodineRepo;
 	
+	@Autowired
+	IndeksRepository indeksRepo;
+	
 	public ObnovaGodine saveObnovaGodine(List<Predmet> predmeti, String datum, String napomena, Indeks indeks) {
 		ObnovaGodine obnovaGodine = new ObnovaGodine();
 		obnovaGodine.setPredmeti(predmeti);
@@ -24,6 +28,11 @@ public class ObnovaGodineService {
 		int date = Integer.parseInt(datum);
 		obnovaGodine.setGodinaObnove(date);
 		obnovaGodine.setIndeks(indeks);
+		
+		Indeks i = new Indeks();
+		i.setObnovaGodine(obnovaGodine);
+		indeksRepo.save(i);
+		
 		return obnovaGodineRepo.save(obnovaGodine);
 	}
 }
